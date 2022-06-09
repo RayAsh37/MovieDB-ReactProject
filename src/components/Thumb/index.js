096dd5821fe1e262c2917e73bc5aef22529515bb
Ashray Shetty <ashrayshetty1999@gmail.com>
2022-06-09 17:56:27 +0530

    completed the Movies.js
    Completed the functionality to show individual movie info

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 //styles
-import { Image } from "./Thumb.styles";
+import { Image } from './Thumb.styles'
 
-const Thumb = ({image, movieId, clickable}) => (
-    <div>
+const Thumb = ({ image, movieId, clickable }) => (
+  <div>
+    {clickable ? (
+      <Link to={`/${movieId}`}>
         <Image src={image} alt='movie-thumb' />
-    </div>
+      </Link>
+    ) : (
+      <Image src={image} alt='movie-thumb' />
+    )}
+  </div>
 )
 
-export default Thumb;
+export default Thumb
